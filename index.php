527557e9804164950cdd231b79460c4d96d2d34c
Juan Zerpa <67330194+JuanZerpa@users.noreply.github.com>
2022-02-06 23:29:02 -0300

    primer cambio

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primero</title>
+</head>
+<body>
+    <?php
+        echo "Hola mundo";
+    ?>
+</body>
 </html>
